7515d35f7f19c7f873c09efd1885fa5132062cba
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-07-13 19:49:38 -0400

    Started to get input

--- temp_change/src/main.rs
+++ temp_change/src/main.rs
@@ -1,8 +1,14 @@
+use std::io;
+
 fn main() {
     println!("Pick what you are converting FROM:");
     println!("1. Celsius");
     println!("2. Fahrenheit");
     //Get input here
+    let mut selection = String::new();
+    io::stdin()
+        .read_line(&mut selection)
+        .expect("Could not read input");
     println!("Put in temperature to be converted:");
     //Get other input here
 }
