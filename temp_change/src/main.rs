f0262ec8a7c1ca082177c5ec3703946deb38bb21
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-07-15 13:12:50 -0400

    Fixed typo and math errors

--- temp_change/src/main.rs
+++ temp_change/src/main.rs
@@ -5,20 +5,12 @@ fn main() {
         println!("Pick what you are converting FROM:");
         println!("1. Celsius");
         println!("2. Fahrenheit");
-        println!("Typing anything but 1 or two causes this program to exit.");
+        println!("Typing anything but 1 or 2 causes this program to exit.");
         let choice = get_choice(2);
         if choice == 0 {
             break;
         }
-        println!("Put in temperature to be converted:");
-        let mut temp = String::new();
-        io::stdin()
-            .read_line(&mut temp)
-            .expect("Could not read input");
-        let temp: f32 = match temp.trim().parse() {
-            Ok(num) => num,
-            Err(_) => 0.0,
-        };
+        let temp = get_temp();
         let result = convert_temp(choice, temp);
         println!("The temp is: {}", result);
     }
@@ -40,10 +32,29 @@ fn get_choice(max: u8) -> u8 {
     }
 }
 
+fn get_temp() -> f32 {
+    loop{
+        println!("Put in temperature to be converted:");
+        let mut temp = String::new();
+            io::stdin()
+                .read_line(&mut temp)
+                .expect("Could not read input");
+            let temp: f32 = match temp.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            return temp;
+        }
+}
+
 fn convert_temp(version: u8, temp: f32) -> f32 {
     if version == 1 {
-        return temp * (9.0 / 5.0) + 32.0
+        let result = temp * (9.0 / 5.0);
+        let result = result + 32.0;
+        return result
     } else {
-        return temp - 32.0 * (5.0 / 9.0)
+        let result = temp - 32.0;
+        let result = result * (5.0 / 9.0);
+        return result
     }
 }
