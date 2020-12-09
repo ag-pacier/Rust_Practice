c725838484ab90b1a7ca9a24413858f379d13166
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-12-09 08:12:21 -0500

    Started/finished strings

--- sand_box/src/strings.rs
+++ sand_box/src/strings.rs
@@ -2,10 +2,41 @@
 // String = Growable, heap-allocated data structure -- use when you need to modify or own string data
 
 pub fn run() {
-    let hello = String::from("Hello");
+    let mut hello = String::from("Hello ");
 
     // Get length
     println!("Length: {}", hello.len());
+    // push char
+    hello.push('W');
+    //push str
+    hello.push_str("orld!");
+
+    // Capacity in bytes
+    println!("Capacity: {}", hello.capacity());
+    //check if empty
+    println!("Is Empty: {}", hello.is_empty());
+
+    //contains
+    println!("Contains 'World': {}", hello.contains("World"));
+
+    // replace
+    println!("Replace: {}", hello.replace("World", "There"));
+
+    //Loop through string by whitespace
+    for word in hello.split_whitespace() {
+        println!("{}", word);
+    }
+
+    // create string with capacity
+    let mut s = String::with_capacity(10);
+    s.push('a');
+    s.push('b');
+
+    // Assertion testing
+    assert_eq!(2, s.len());
+    assert_eq!(10, s.capacity());
+
+    println!("{}", s);
 
     println!("{}", hello);
 }
