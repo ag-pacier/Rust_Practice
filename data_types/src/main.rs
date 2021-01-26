3df949fa507b75a4b16df45af3cfe4b69d98d95f
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2021-01-26 12:08:56 -0500

    Followed along in chapter 3.2

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 2.0; //defaults to type f64
+    let y: f32 = 3.0; //set to f32 since we annoted it
+
+    println!("x is {} and y is {}", x, y);
+
+    // addition
+    let sum = x + y;
+    // subtraction
+    let difference = x - y;
+    // multiplication
+    let product = x * y;
+    // division
+    let quotient = x / y;
+    // remainder/modulo
+    let remainder = y % x;
+
+    println!("Sum: {}, Difference: {}, Product: {}, Division: {}, Remainder: {}", sum, difference, product, quotient, remainder);
 }
