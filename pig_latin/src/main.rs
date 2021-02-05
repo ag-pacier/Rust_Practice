5aaac3675c90b01307b58fc8184a52913c42241e
Gage Pacier <gage@technicallythoughts.com>
2021-02-05 16:29:39 -0500

    Started pig latin challenge

---
+++ pig_latin/src/main.rs
@@ -0,0 +1,13 @@
+
+fn pig_latin_word(word: &str) -> String {
+    let mut new_word = String::new();
+    let mut first_letter: char;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    if vowels.contains(word.chars().next())
+
+}
+
+fn main() {
+    //Convert strings to pig latin
+
+}
