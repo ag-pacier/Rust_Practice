40c61ab750fe88b52e93fde3b71e434c25aa7169
Gage Pacier <gage@technicallythoughts.com>
2021-02-06 22:09:50 -0500

    Finished 2nd exercise in 8

--- pig_latin/src/main.rs
+++ pig_latin/src/main.rs
@@ -1,13 +1,24 @@
-
 fn pig_latin_word(word: &str) -> String {
     let mut new_word = String::new();
-    let mut first_letter: char;
+    let first_letter = &word.chars().next().unwrap_or_default().to_ascii_lowercase();
     let vowels = ['a', 'e', 'i', 'o', 'u'];
-    if vowels.contains(word.chars().next())
-
+    if vowels.contains(&first_letter) {
+        new_word.push_str(&word);
+        new_word.push_str("-hay");
+    } else {
+        new_word.push_str(&word[1..]);
+        new_word.push_str("-");
+        new_word.push(*first_letter);
+        new_word.push_str("ay");
+    };
+    new_word
 }
 
 fn main() {
     //Convert strings to pig latin
+    let test1 = String::from("Apple");
+    let test2 = String::from("Jerk");
+    let test3 = String::from("Muppet");
 
+    println!("Apple: {}, Jerk: {}, Muppet: {}", pig_latin_word(&test1), pig_latin_word(&test2), pig_latin_word(&test3));
 }
