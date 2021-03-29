2254e3a28792b822496f250142a2e10a13c5b083
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2021-03-29 09:11:21 -0400

    Working on enums again
    It didn't sink in the first time

--- enummerate/src/main.rs
+++ enummerate/src/main.rs
@@ -3,12 +3,18 @@ enum IpAddr {
     V6(String),
 }
 //enum is a custom data type like String
+//It differs from struct in that the data type doesn't always need to be the same
+
+enum IpAddress {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
 
 fn main() {
     //Starting on section 6.1
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    //let home = IpAddr::V4(String::from("127.0.0.1"));
 
-    let loopback = IpAddr::V6(String::from("::1"));
+    //let loopback = IpAddr::V6(String::from("::1"));
 
 }
