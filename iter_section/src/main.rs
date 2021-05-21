a40bca03d75e999f6c343588fc2bae6e67ffccad
Gage Pacier <lead.diet@gmail.com>
2021-05-21 12:00:35 -0400

    got a little further

--- iter_section/src/main.rs
+++ iter_section/src/main.rs
@@ -5,5 +5,12 @@ fn main() {
 
     for val in v1_iter {
         println!("Got: {}", val);
+        //nom nom nom, consuming the iterator, sortof
     }
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+    //Iterators are "lazy" and need to be consumed. That's what collect is doing.
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
 }
