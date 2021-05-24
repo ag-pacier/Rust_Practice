6d3955703ba3019d9658410e3fdae13a63825743
Gage Pacier <lead.diet@gmail.com>
2021-05-24 08:38:56 -0400

    Finished 15.1

--- box_section/src/main.rs
+++ box_section/src/main.rs
@@ -1,5 +1,22 @@
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
     // Box is a struct that puts a pointer on the stack to a piece of data on the heap
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // Normally the compiler is going to look at the largest possible variant of the enum to ensure it can
+    // makes space for it. Using Box<T> Rust knows it'll always be the same amount of space in that it will
+    // always be a pointer to some data on the heap
+
+    // Box<T> implements the Drop trait which means after it goes out of scope, Rust can clean up the data
+    // It also implements the Deref trait so that the data on the heap can be pulled
+
+    
 }
